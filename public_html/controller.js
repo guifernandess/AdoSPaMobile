5bd0a9449a97b2cb81693debd8af26878eab9926
guilherme.rocha <guilherme.rocha@172.16.51.157>
2018-11-17 11:27:09 -0200

    Ajustes
    corrigir função remover tarefas

--- public_html/controller.js
+++ public_html/controller.js
@@ -19,24 +19,37 @@ app.config(function ($stateProvider, $urlRouterProvider) {
 
 app.controller('PrincipalCtrl', function ($scope, $tarefaDao) {
 
-    $scope.tarefas = [];
+    $scope.tarefas = $tarefaDao.getTarefas();
+    $scope.mostrarTerminadas = true;
     
     $tarefaDao.getTarefas().then(function (tarefas) {
         $scope.tarefas = tarefas;
     });
+    
+    $scope.filtroTarefas = function(tarefa) {
+        if (tarefa.status == true && $scope.mostrarTerminadas == false) {
+            return false;
+        }
+        return true;
+    };
+    
+    $scope.filtrarTarefasTerminadas = function () {
+        $tarefaDao.filtroTarefas();
+    };
+
 });
 
-app.controller('ProdutoCtrl', function ($scope, $tarefaDao, $stateParams) {
-
-    $scope.produto = $tarefaDao.getTarefaById($stateParams.id);
-
-});
+//app.controller('TarefaCtrl', function ($scope, $tarefaDao, $stateParams) {
+//
+//    $scope.produto = $tarefaDao.getTarefaById($stateParams.id);
+//
+//});
 
 
 app.factory('$tarefaDao', function ($http, $q) {
 
     var tarefas = [];
-
+    
     function getTarefas() {
         return $q(function (resolve, reject) {
             $http.get('tarefas.json').then(function (response) {
@@ -60,5 +73,14 @@ app.factory('$tarefaDao', function ($http, $q) {
         getTarefas,
         getTarefaById
     };
+
+
+    function removeTarefasTerminadas() {
+        var temp = tarefas.filter((tarefa) => {
+            return (!tarefa.status);
+        });
+        tarefas = temp;
+
+    }
 
 });
